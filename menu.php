2a617f15b282a2f679b77a56f0766de1c3a77c9a
kieuhann <105109200+kieuhann@users.noreply.github.com>
2023-09-19 18:35:37 +0700

    menu lần 2

--- menu.php
+++ menu.php
@@ -24,7 +24,7 @@ $activate = "menu";
 	            <div class="nav nav-pills d-flex text-center" id="v-pills-tab" role="tablist" aria-orientation="vertical">
 	              <a class="nav-link ftco-animate active" id="v-pills-1-tab" data-toggle="pill" href="#v-pills-1" role="tab" aria-controls="v-pills-1" aria-selected="true">Combo</a>
 
-	              <a class="nav-link ftco-animate" id="v-pills-2-tab" data-toggle="pill" href="#v-pills-2" role="tab" aria-controls="v-pills-2" aria-selected="false">Sốt</a>
+	              <a class="nav-link ftco-animate" id="v-pills-2-tab" data-toggle="pill" href="#v-pills-2" role="tab" aria-controls="v-pills-2" aria-selected="false">Sốt và các món khai vị</a>
 
 	              <a class="nav-link ftco-animate" id="v-pills-3-tab" data-toggle="pill" href="#v-pills-3" role="tab" aria-controls="v-pills-3" aria-selected="false">Các loại thịt</a>
 
@@ -55,7 +55,7 @@ $activate = "menu";
 							                  <span class="price">169.000đ</span>
 							                </div>
 							              </div>
-							              <p><span>Meat</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
+							              <p><span>Thịt bò Mỹ</span>, <span>Tôm</span>, <span>Mực</span>, <span>2 Món khai vị</span></p>
 							              <p><a href="#" class="btn btn-primary">Đặt ngay</a></p>
 						              </div>
 					              </div>
@@ -74,7 +74,7 @@ $activate = "menu";
 							                  <span class="price">219.000đ</span>
 							                </div>
 							              </div>
-							              <p><span>Meat</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
+							              <p><span>Thịt bò Mỹ</span>, <span>Tôm</span>, <span>Mực</span>, <span>2 món khai vị</span></p>
 							              <p><a href="#" class="btn btn-primary">Đặt ngay</a></p>
 						              </div>
 					              </div>
@@ -94,7 +94,7 @@ $activate = "menu";
 							                  <span class="price">239.00đ</span>
 							                </div>
 							              </div>
-							              <p><span>Meat</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
+							              <p><span>Thịt bò Mỹ</span>, <span>Tôm</span>, <span>Mực</span>, <span>Gà</span,<span>3 món khai vị</span>></p>
 							              <p><a href="#" class="btn btn-primary">Đặt ngay</a></p>
 						              </div>
 					              </div>
@@ -113,7 +113,7 @@ $activate = "menu";
 							                  <span class="price">239.000đ</span>
 							                </div>
 							              </div>
-							              <p><span>Meat</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
+							              <p><span>Thịt bò Mỹ</span>, <span>Tôm</span>, <span>Nghêu</span>, <span>Cá</span></p>
 							              <p><a href="#" class="btn btn-primary">Đặt ngay</a></p>
 						              </div>
 					              </div>
@@ -133,7 +133,7 @@ $activate = "menu";
 							                  <span class="price">259.000đ</span>
 							                </div>
 							              </div>
-							              <p><span>Meat</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
+							              <p><span>Thịt bò Mỹ</span>, <span>Cá</span>, <span>Tôm</span>, <span>Mực</span>,<span>Nghêu</span></p>
 							              <p><a href="#" class="btn btn-primary">Đặt ngay</a></p>
 						              </div>
 					              </div>
@@ -152,7 +152,7 @@ $activate = "menu";
 							                  <span class="price">289.000đ</span>
 							                </div>
 							              </div>
-							              <p><span>Meat</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
+							              <p><span>Thịt bò Mỹ</span>, <span>Tôm</span>, <span>Cá</span>, <span>Mực</span>,<span>Nghêu</span>,<span>3 món khai vị</span></p>
 							              <p><a href="#" class="btn btn-primary">Đặt ngay</a></p>
 						              </div>
 					              </div>
@@ -172,7 +172,7 @@ $activate = "menu";
 							                  <span class="price">299.000đ</span>
 							                </div>
 							              </div>
-							              <p><span>Meat</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
+							              <p><span>Thị bò Mỹ</span>, <span>Tôm</span>, <span>Mực</span>, <span>Chân gà</span>,<span>3 món khai vị</span></p>
 							              <p><a href="#" class="btn btn-primary">Đặt ngay</a></p>
 						              </div>
 					              </div>
@@ -185,13 +185,13 @@ $activate = "menu";
 													<div>
 						              	<div class="d-flex">
 							                <div class="one-half">
-							                  <h3>Grilled Beef with potatoes</h3>
+							                  <h3>Combo Nướng dành cho 3 người</h3>
 							                </div>
 							                <div class="one-forth">
-							                  <span class="price">$29</span>
+							                  <span class="price">289.000đ</span>
 							                </div>
 							              </div>
-							              <p><span>Meat</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
+							              <p><span>Thịt bò</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
 							              <p><a href="#" class="btn btn-primary">Đặt ngay</a></p>
 						              </div>
 					              </div>
@@ -209,13 +209,13 @@ $activate = "menu";
 													<div>
 						              	<div class="d-flex">
 							                <div class="one-half">
-							                  <h3>Grilled Beef with potatoes</h3>
+							                  <h3>Sốt chấm</h3>
 							                </div>
 							                <div class="one-forth">
-							                  <span class="price">$29</span>
+							                  <span class="price">0đ</span>
 							                </div>
 							              </div>
-							              <p><span>Meat</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
+							              <p><span>Tương đen</span>, <span>Cà ri</span>, <span>Kem</span></p>
 							              <p><a href="#" class="btn btn-primary">Đặt ngay</a></p>
 						              </div>
 					              </div>
@@ -228,13 +228,13 @@ $activate = "menu";
 													<div>
 						              	<div class="d-flex">
 							                <div class="one-half">
-							                  <h3>Grilled Beef with potatoes</h3>
+							                  <h3>Salad khai vị</h3>
 							                </div>
 							                <div class="one-forth">
-							                  <span class="price">$29</span>
+							                  <span class="price">59.000đ</span>
 							                </div>
 							              </div>
-							              <p><span>Meat</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
+							              <p><span>Salad</span>, <span>Cà chua</span>, <span>phô mai</span>, <span>Ớt chuông</span></p>
 							              <p><a href="#" class="btn btn-primary">Đặt ngay</a></p>
 						              </div>
 					              </div>
@@ -248,13 +248,13 @@ $activate = "menu";
 													<div>
 						              	<div class="d-flex">
 							                <div class="one-half">
-							                  <h3>Grilled Beef with potatoes</h3>
+							                  <h3>Súp kem bí đỏ</h3>
 							                </div>
 							                <div class="one-forth">
-							                  <span class="price">$29</span>
+							                  <span class="price">79.000đ</span>
 							                </div>
 							              </div>
-							              <p><span>Meat</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
+							              <p><span>Bí đỏ</span>, <span>Kem tươi</span></p>
 							              <p><a href="#" class="btn btn-primary">Đặt ngay</a></p>
 						              </div>
 					              </div>
@@ -267,13 +267,13 @@ $activate = "menu";
 													<div>
 						              	<div class="d-flex">
 							                <div class="one-half">
-							                  <h3>Grilled Beef with potatoes</h3>
+							                  <h3>Há cảo</h3>
 							                </div>
 							                <div class="one-forth">
-							                  <span class="price">$29</span>
+							                  <span class="price">89.000đ</span>
 							                </div>
 							              </div>
-							              <p><span>Meat</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
+							              <p><span>Tôm</span>, <span>Thịt</span>, <span>Nấm </span>, <span>Bắp</span>,<span>hành tím</span></p>
 							              <p><a href="#" class="btn btn-primary">Đặt ngay</a></p>
 						              </div>
 					              </div>
@@ -287,13 +287,13 @@ $activate = "menu";
 													<div>
 						              	<div class="d-flex">
 							                <div class="one-half">
-							                  <h3>Grilled Beef with potatoes</h3>
+							                  <h3>Súp kem phô mai</h3>
 							                </div>
 							                <div class="one-forth">
-							                  <span class="price">$29</span>
+							                  <span class="price">109.000đ</span>
 							                </div>
 							              </div>
-							              <p><span>Meat</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
+							              <p><span>Thịt bò</span>, <span>Phô mai</span>, <span>Hành tây</span>, <span>Bánh mỳ</span></p>
 							              <p><a href="#" class="btn btn-primary">Đặt ngay</a></p>
 						              </div>
 					              </div>
@@ -306,13 +306,13 @@ $activate = "menu";
 													<div>
 						              	<div class="d-flex">
 							                <div class="one-half">
-							                  <h3>Grilled Beef with potatoes</h3>
+							                  <h3>Gỏi mực Thái Lan</h3>
 							                </div>
 							                <div class="one-forth">
-							                  <span class="price">$29</span>
+							                  <span class="price">89.000đ</span>
 							                </div>
 							              </div>
-							              <p><span>Meat</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
+							              <p><span>Mực</span>, <span>Dưa leo</span>, <span>cà rốt</span>, <span>Ớt</span></p>
 							              <p><a href="#" class="btn btn-primary">Đặt ngay</a></p>
 						              </div>
 					              </div>
@@ -326,13 +326,13 @@ $activate = "menu";
 													<div>
 						              	<div class="d-flex">
 							                <div class="one-half">
-							                  <h3>Grilled Beef with potatoes</h3>
+							                  <h3>Sủi cảo</h3>
 							                </div>
 							                <div class="one-forth">
-							                  <span class="price">$29</span>
+							                  <span class="price">79.000đ</span>
 							                </div>
 							              </div>
-							              <p><span>Meat</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
+							              <p><span>Thịt</span>, <span>Tôm</span>, <span>Bắp cải</span>, <span>Đậu phụ</span></p>
 							              <p><a href="#" class="btn btn-primary">Đặt ngay</a></p>
 						              </div>
 					              </div>
@@ -345,13 +345,13 @@ $activate = "menu";
 													<div>
 						              	<div class="d-flex">
 							                <div class="one-half">
-							                  <h3>Grilled Beef with potatoes</h3>
+							                  <h3>Bánh bao chiên</h3>
 							                </div>
 							                <div class="one-forth">
-							                  <span class="price">$29</span>
+							                  <span class="price">59.000đ</span>
 							                </div>
 							              </div>
-							              <p><span>Meat</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
+							              <p><span>Thịt</span>, <span>Củ sắn</span>, <span>Trứng </span>, <span>Nấm</span></p>
 							              <p><a href="#" class="btn btn-primary">Đặt ngay</a></p>
 						              </div>
 					              </div>
@@ -369,13 +369,13 @@ $activate = "menu";
 													<div>
 						              	<div class="d-flex">
 							                <div class="one-half">
-							                  <h3>Grilled Beef with potatoes</h3>
+							                  <h3>Thịt bò Mỹ</h3>
 							                </div>
 							                <div class="one-forth">
-							                  <span class="price">$29</span>
+							                  <span class="price">59.000đ</span>
 							                </div>
 							              </div>
-							              <p><span>Meat</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
+							              <p><span>Thịt bò Mỹ</span></p>
 							              <p><a href="#" class="btn btn-primary">Đặt ngay</a></p>
 						              </div>
 					              </div>
@@ -388,13 +388,13 @@ $activate = "menu";
 													<div>
 						              	<div class="d-flex">
 							                <div class="one-half">
-							                  <h3>Grilled Beef with potatoes</h3>
+							                  <h3>Thịt thăn vai</h3>
 							                </div>
 							                <div class="one-forth">
-							                  <span class="price">$29</span>
+							                  <span class="price">69.000đ</span>
 							                </div>
 							              </div>
-							              <p><span>Meat</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
+							              <p><span>Thịt thăn vai</span></p>
 							              <p><a href="#" class="btn btn-primary">Đặt ngay</a></p>
 						              </div>
 					              </div>
@@ -408,13 +408,13 @@ $activate = "menu";
 													<div>
 						              	<div class="d-flex">
 							                <div class="one-half">
-							                  <h3>Grilled Beef with potatoes</h3>
+							                  <h3>Thịt bắp bò Mỹ</h3>
 							                </div>
 							                <div class="one-forth">
-							                  <span class="price">$29</span>
+							                  <span class="price">59.000đ</span>
 							                </div>
 							              </div>
-							              <p><span>Meat</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
+							              <p><span>Thịt bắp bò Mỹ</span></p>
 							              <p><a href="#" class="btn btn-primary">Đặt ngay</a></p>
 						              </div>
 					              </div>
@@ -427,13 +427,13 @@ $activate = "menu";
 													<div>
 						              	<div class="d-flex">
 							                <div class="one-half">
-							                  <h3>Grilled Beef with potatoes</h3>
+							                  <h3>Thăn ngoại bò Wagyu</h3>
 							                </div>
 							                <div class="one-forth">
-							                  <span class="price">$29</span>
+							                  <span class="price">79.000đ</span>
 							                </div>
 							              </div>
-							              <p><span>Meat</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
+							              <p><span>Thăn ngoại bò Wagyu</span></p>
 							              <p><a href="#" class="btn btn-primary">Đặt ngay</a></p>
 						              </div>
 					              </div>
@@ -447,13 +447,13 @@ $activate = "menu";
 													<div>
 						              	<div class="d-flex">
 							                <div class="one-half">
-							                  <h3>Grilled Beef with potatoes</h3>
+							                  <h3>Thịt bò hữu cơ Obe</h3>
 							                </div>
 							                <div class="one-forth">
-							                  <span class="price">$29</span>
+							                  <span class="price">89.000đ</span>
 							                </div>
 							              </div>
-							              <p><span>Meat</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
+							              <p><span>Thịt bò hữu cơ Obe</span></p>
 							              <p><a href="#" class="btn btn-primary">Đặt ngay</a></p>
 						              </div>
 					              </div>
@@ -466,13 +466,13 @@ $activate = "menu";
 													<div>
 						              	<div class="d-flex">
 							                <div class="one-half">
-							                  <h3>Grilled Beef with potatoes</h3>
+							                  <h3>Thịt ba chỉ bò Mỹ</h3>
 							                </div>
 							                <div class="one-forth">
-							                  <span class="price">$29</span>
+							                  <span class="price">59.000đ</span>
 							                </div>
 							              </div>
-							              <p><span>Meat</span>, <span>Potatoes</span>, <span>Rice</span>, <span>Tomatoe</span></p>
+							              <p><span>Thịt ba chỉ bò Mỹ</span></p>
 							              <p><a href="#" class="btn btn-primary">Đặt ngay</a></p>
 						              </div>
 					              </div>
